1444ab413dfb60525f21d5bb0d28fa802182e07f
sk_courseworks <sk_courseworks@Samanthas-MacBook-Air.local>
2024-02-19 08:36:28 -0500

    remove old project

--- SimplyWealthProject/SimplyWealthApp/urls.py
+++ SimplyWealthProject/SimplyWealthApp/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name='index'),
     path("signup/", views.signup, name='signup'),
-    path("login/", views.login_view, name='login_view'),
+    path("login_view/", views.login_view, name='login_view'),
     path("<str:username>", views.userhome, name='userhome')
 ]
